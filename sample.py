48f19f46d2e25950ac52753df5718ca19078075b
AnikaZN <anikanacey@gmail.com>
2020-06-19 20:56:59 -0600

    Finished!

--- sample.py
+++ sample.py
@@ -3,23 +3,20 @@ from sklearn import metrics
 from sklearn.neighbors import KNeighborsClassifier
 import numpy as np
 
-# dataset
 iris = datasets.load_iris()
-X = iris.data  # we only take the first two features.
+X = iris.data
 y = iris.target
 
-# fit a k-nearest neighbor model to the data
 K = 3
 model = KNeighborsClassifier(n_neighbors = K)
 model.fit(X, y)
-print(model)
 
-# make predictions
-print( '(-2, -2, 2, 2) is class')
-print( model.predict([[-2,-2, 2, 2]]) ) # class 0
+# Make predictions
+print('(-2, -2, 2, 2) is class:')
+print(model.predict(X, [-2, -2, 2, 2]))  # Class 0
 
-print( '(1, 5, 5, 1) is class'),
-print( model.predict([[1, 5, 5, 1]]) ) # class 1
+print('(1, 5, 5, 1) is class:')
+print(model.predict(X, [1, 5, 5, 1]))  # Class 1
 
-print( '(10, 10, 10, 10) is class')
-print( model.predict([[10, 10, 10, 10]]) ) # class 2
+print('(10, 10, 10, 10) is class:')
+print(model.predict(X, [10, 10, 10, 10]))  # Class 2
